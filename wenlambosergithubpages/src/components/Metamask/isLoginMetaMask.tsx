acd92bb207052e1085195b3e3382ee8aea6826e0
Saku Kaarakainen <saku.kaarakainen@innofactor.com>
2022-03-22 16:34:06 +0200

    v2

---
+++ wenlambosergithubpages/src/components/Metamask/isLoginMetaMask.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class isLoginMetaMask extends Component {
+    constructor(props: any) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Login into MetaMask
+                    <br />
+                </p>
+            </div>
+
+        )
+    }
+}
---
+++ wenlambosergithubpages/src/components/Metamask/isMetaMask.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class isMetaMask extends Component {
+
+    constructor(props: any) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <div>
+                <p>Install MetaMask to Sign in</p>
+            </div>
+        )
+    }
+}
