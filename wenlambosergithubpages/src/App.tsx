91531efdec8bcb8fceb629452fd3c92036f534e4
Saku Kaarakainen <saku.kaarakainen@innofactor.com>
2022-03-22 07:26:35 +0200

    working version in pages

--- wenlambosergithubpages/src/App.tsx
+++ wenlambosergithubpages/src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 export default class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <NavBar />
                 <CourseList />
             </BrowserRouter>
