bdcf02be60da8142b76e80d8ea16afc272f08911
Saku Kaarakainen <saku.kaarakainen@innofactor.com>
2022-03-25 17:19:12 +0200

    improved list styles

--- wenlambosergithubpages/src/components/AssetList.tsx
+++ wenlambosergithubpages/src/components/AssetList.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import { Dispatch } from 'redux';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField' // TODO: Use TextField, or not
 
@@ -21,17 +25,21 @@ const AssetList: React.FC = () => {
     );
 
     return (
-        <main>
-            <h1>My Asset</h1>
-            <AddAsset saveAsset={saveAsset} />
-            {assets.map((asset: IAsset) => (
-                <Asset
-                    key={asset.id}
-                    asset={asset}
-                    removeAsset={removeAsset}
-                />
-            ))}
-        </main>
+        <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+            <h2>My Assets</h2>
+            {/*<AddAsset saveAsset={saveAsset} />*/}
+            <List dense={false}>              
+                {/*<Asset key={asset.id} asset={asset} removeAsset={removeAsset} />*/}
+                {/*<ListItemIcon><FolderIcon /> </ListItemIcon>*/}
+                {assets.map((asset: IAsset) => (
+                <ListItem>
+                    <ListItemText
+                            primary = {`${asset.name} (${asset.symbol}): ${asset.amount}`}
+                            secondary={asset.address} />
+                </ListItem>
+                ))}                
+            </List>
+        </Box>
     );
 };
 
